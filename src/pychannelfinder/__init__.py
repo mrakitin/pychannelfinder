a3b3648a5c902e6a044f7b29bbb123b2ec4004a6
Max Rakitin <mrakitin@bnl.gov>
2024-06-12 15:28:38 -0400

    CI: fix tests and run on ubuntu only for now

--- src/pychannelfinder/__init__.py
+++ src/pychannelfinder/__init__.py
@@ -3,8 +3,10 @@
 from __future__ import annotations
 
 from .client import AuthenticatedClient, Client
+from ._version import version as __version__
 
 __all__ = (
     "AuthenticatedClient",
     "Client",
+    "__version__"
 )
