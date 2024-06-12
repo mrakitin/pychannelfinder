ad2d27ead7a04805fd0b014a607fe58dfd93d6f0
Max Rakitin <mrakitin@bnl.gov>
2024-06-12 15:31:30 -0400

    STY: fix style

--- src/pychannelfinder/__init__.py
+++ src/pychannelfinder/__init__.py
@@ -2,11 +2,7 @@
 
 from __future__ import annotations
 
-from .client import AuthenticatedClient, Client
 from ._version import version as __version__
+from .client import AuthenticatedClient, Client
 
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-    "__version__"
-)
+__all__ = ("AuthenticatedClient", "Client", "__version__")
